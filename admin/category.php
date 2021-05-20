5fc355b89aa452a6eb8f1e6b2aea993a98735983
fessouma1 <80673653+fessouma1@users.noreply.github.com>
2021-05-20 19:47:00 -0400

    admin page

---
+++ admin/category.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include("adminpartial/head.php");
+?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+<?php
+include("adminpartial/header.php");
+include("adminpartial/aside.php");
+
+?>
+ 
+  <!-- Left side column. contains the logo and sidebar -->
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+     <div class="row">
+       <div class="col-sm-3">
+
+</div>
+
+<div class="col-sm-6">
+
+
+       <form role="form">
+         <h1>Categories</h1>
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="category">Categories</label>
+                  <input type="text" class="form-control" id="category" placeholder="Enter category">
+                </div>
+               
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+
+            </form>
+            </div>
+            <div class=col-sm-3>
+
+
+              </div>
+        </div>
+    </section></div>
+    <!-- /.content -->
+    </div>
+  <!-- /.content-wrapper -->
+ <?php
+    include("adminpartial/footer.php");
+ ?>
+</body>
+</html>
