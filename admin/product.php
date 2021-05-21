804a85c51671f73bcd031d43d38150012315f73d
fessouma1 <80673653+fessouma1@users.noreply.github.com>
2021-05-20 23:30:23 -0400

    design the product page
    admin can now add new product to the inventory.

---
+++ admin/product.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include("adminpartial/head.php");
+?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+<?php
+include("adminpartial/header.php");
+include("adminpartial/aside.php");
+
+?>
+ 
+  <!-- Left side column. contains the logo and sidebar -->
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+     <div class="row">
+       <div class="col-sm-3">
+
+</div>
+
+<div class="col-sm-6">
+
+
+       <form role="form">
+       <h1>Products</h1>
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="name">Name</label>
+                  <input type="text" class="form-control" id="name" placeholder="Enter Product Name">
+                </div>
+                <div class="form-group">
+                  <label for="price">Price</label>
+                  <input type="text" class="form-control" id="price" placeholder="price">
+                </div>
+                <div class="form-group">
+                  <label for="picture">File input</label>
+                  <input type="file" id="picture">
+                </div>
+                <div class="form-group">
+                  <label for="description">Description</label>
+                  <textarea id="description" class="form-group-control" row="10" placeholder="Enter Description"></textarea>
+                </div>
+                <div class="form-group">
+                <label for="category">category</label>
+                <select id="category">
+                <option>Shirt</option>
+                <option>Pants</option>
+                </select>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+
+            </form>
+            </div>
+            <div class=col-sm-3>
+
+
+              </div>
+        </div>
+    </section></div>
+    <!-- /.content -->
+    </div>
+  <!-- /.content-wrapper -->
+ <?php
+    include("adminpartial/footer.php");
+ ?>
+</body>
+</html>
